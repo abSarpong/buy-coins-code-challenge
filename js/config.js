822494eaf9a3369dba96491a1539401177308a80
Michael Sarpong <mike.sarp.ky@gmail.com>
2020-11-27 21:53:16 +0000

    add token

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const gitHub_token = { // this should be const instead if you're using ES6 standards
-    token : 'a8ff3e236be61ec0d9e2dc2e304edab55e6afb46'
+    token : 'fbc1625d0af641b3b55c412927dbc9e3d0cc2d8d'
 }
